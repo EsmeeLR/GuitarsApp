93e1e524830ca66289498f642afae181dd35be91
EsmeeLR <esmeralda.lunaromano14mail.com>
2025-10-19 10:04:23 -0600

    guitarApp04

--- js/app.js
+++ js/app.js
@@ -1,26 +1,53 @@
 import { db } from "./guitarras.js"
 
+const carrito = []
+
 const divContainer = document.querySelector('main div')
 
 const createCard = (guitar) => {
     const div = document.createElement('div')
     div.className = 'col-md-6 col-lg-4 my-4 row align-items-center'
     const html = `<div class="col-4">
-                    <img class="img-fluid" src="./img/guitarra_01.jpg" alt="imagen guitarra">
+                    <img class="img-fluid" src="./img/${guitar.imagen}.jpg" alt="imagen guitarra">
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">${guitar.nombre}</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
+                    <p>${guitar.descripcion}</p>
+                    <p class="fw-black text-primary fs-3">${guitar.precio}</p>
                     <button 
+                        data-id="${guitar.id}"
                         type="button"
                         class="btn btn-dark w-100 "
                     >Agregar al Carrito</button>
                 </div>`
+
     div.innerHTML = html
     return div
+}
+
+const buttonClicked = (e) => {
+    if(e.target.classList.contains('btn')){
+        const dataId = e.target.getAttribute('data-id')
+        // Verificar si existe guitar en carrito
+        const idCarrito = carrito .findIndex(g => g.id === Number(dataId))
+        //Si no, crea un objeto nuevo
+        if(idCarrito === -1){
+            carrito.push({
+                ...db[Number(dataId) - 1],
+                cantidad: 1
+            })
+        } else {
+            //Si si incrementa cantidad
+            carrito[idCarrito].cantidad++
+        }
+        //Si sí, incrementa cantidad
+
+        console.log(carrito)
+    }
 }
 db.forEach((guitar) => {
     console.log(guitar.nombre)
     divContainer.appendChild(createCard(guitar))
 })
+
+divContainer.addEventListener('click', buttonClicked)
